e1846e14c16c6274a5b9e0e6106d583b0df3f479
gankedbybus <42853427+gankedbybus@users.noreply.github.com>
2021-09-05 16:18:21 +1000

    Initial commit

---
+++ HackerRank/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+partial class Solution
+{
+    public static string _testCase = "03";
+    static void Main(string[] args)
+    {
+        var (inputFileName, myOutputFileName, expectedOutputFileName) = GetFileNames(_testCase);
+        Console.SetIn(new StreamReader(inputFileName));
+        HackerRankCode();
+        Console.WriteLine(CompareMyOutputWithExpectedOutput(myOutputFileName, expectedOutputFileName));
+    }
+
+    // insert hackerrank main code here
+    public static void HackerRankCode()
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        Result.countSwaps(a);
+    }
+
+    // insert hackerrank class & function below
+    class Result
+    {
+        /*
+         * Complete the 'countSwaps' function below.
+         *
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+
+        public static void countSwaps(List<int> a)
+        {
+            var swapCount = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                for (int j = 0; j < a.Count - 1; j++)
+                {
+                    // Swap adjacent elements if they are in decreasing order
+                    if (a[j] > a[j + 1])
+                    {
+                        var temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        swapCount++;
+                    }
+                }
+            }
+
+            // verify locally that stuff passes testcases
+            WriteLinesToFile(new List<string>
+            {
+                $"Array is sorted in {swapCount} swaps.",
+                $"First Element: {a.First()}",
+                $"Last Element: {a.Last()}"
+            });
+        }
+    }
+}
---
+++ HackerRank/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+partial class Solution
+{
+    private static bool CompareMyOutputWithExpectedOutput(string myOutputFileName, string expectedOutputFileName)
+    {
+        try
+        {
+            var myOutputFile = File.ReadAllLines(myOutputFileName);
+            var expectedOutputFile = File.ReadAllLines(expectedOutputFileName);
+            for (var i = 0; i < myOutputFile.Length; i++)
+            {
+                if (!expectedOutputFile[i].Equals(myOutputFile[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    static (string inputFileName, string myOutputFileName, string expectedOutputFileName) GetFileNames(string testNumber)
+    {
+        return (inputFileName: Path.Combine(GetSolutionPath, GetProblemName, $@"input\input{testNumber}.txt"),
+                myOutputFileName: GetOutputFileName(testNumber),
+                expectedOutputFileName: Path.Combine(GetSolutionPath, GetProblemName, $@"output\output{testNumber}.txt"));
+    }
+
+    static string GetOutputFileName(string testNumber)
+    {
+        return Path.Combine(GetSolutionPath, GetProblemName, $@"output\myoutput{testNumber}.txt");
+    }
+
+    static string GetSolutionPath => Environment.GetEnvironmentVariable("BASE_PATH");
+    static string GetProblemName => Environment.GetEnvironmentVariable("PROBLEM");
+    static void WriteLinesToFile(List<string> input)
+    {
+        var myOutputFileName = GetOutputFileName(_testCase);
+        using (var textWriter = new StreamWriter(myOutputFileName))
+        {
+            foreach (var line in input)
+            {
+                textWriter.WriteLine(line);
+            }
+        }
+    }
+}
